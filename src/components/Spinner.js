2a8465660e391be3e2a120ddc68f4c18f27230ad
Vedu-07 <vedantswami02@gmail.com>
2024-06-22 09:59:39 +0530

    Simple Shopping Cart Created using Redux ToolKit

---
+++ src/components/Spinner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Spinner.css";
+
+const Spinner = () => {
+  return (
+    <div className="w-[100vw] ml-[50%]">
+      <div className="spinner"></div>
+    </div>
+  );
+};
+
+export default Spinner;
